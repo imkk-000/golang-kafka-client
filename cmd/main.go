ed8b7e7fa25f3fb05f7cd593b9bde7f19c8fc2c1
Nattakit Boonyang <nattakit.boonyang@gmail.com>
2019-10-07 21:09:10 +0700

    Add: prototype for marshaling protobuf message

---
+++ cmd/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"go-kafka-client/model"
+	"log"
+	"github.com/golang/protobuf/proto"
+)
+
+func main() {
+	message := &model.Message{
+		Id: proto.Int32(1),
+		Name: proto.String("Foo Bar"),
+		Email: proto.String("foo@bar.bac"),
+	}
+	data, err := proto.Marshal(message)
+	if err != nil {
+		log.Fatal("marshaling error: ", err)
+	}
+	log.Println(data)
+	log.Println(string(data))
+}
